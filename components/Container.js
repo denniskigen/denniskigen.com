102f75ca12f2e5a9ec8e50d7fc50c6b8c0d0f830
Dennis Kigen <kigen.work@gmail.com>
2021-10-27 23:10:33 +0300

    Show nav links on all viewports

--- components/Container.js
+++ components/Container.js
@@ -15,7 +15,7 @@ function NavItem({ href, text }) {
           isActive
             ? "font-semibold text-gray-800 dark:text-gray-200 bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-900 dark:hover:text-gray-400"
             : "font-normal text-gray-600 dark:text-gray-400"
-        } hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all`}
+        } inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all`}
       >
         <span className="capsize">{text}</span>
       </a>
@@ -62,7 +62,7 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <div className="flex flex-col justify-center px-8  max-w-3xl mx-auto">
+      <div className="flex flex-col justify-center px-8 max-w-3xl mx-auto">
         <nav className="flex items-center justify-between w-full relative border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16 text-gray-900 bg-opacity-60 dark:text-gray-100">
           <span className="font-bold tracking-wide uppercase">
             Dennis Kigen
