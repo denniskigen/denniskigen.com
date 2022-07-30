e1a55fb8c6c39d531bdd014c5dae3ddd635a3be0
Dennis Kigen <kigen.work@gmail.com>
2022-07-30 11:18:46 +0300

    Add opacity to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
     <>
       <div
         data-active={scrolled}
-        className="sticky right-0 left-0 top-0 z-10 flex flex-col items-center backdrop-blur h-16 text-very-grey dark:text-very-light"
+        className="sticky right-0 left-0 top-0 z-10 flex flex-col items-center backdrop-blur h-16 opacity-95 text-very-grey dark:text-very-light"
       ></div>
     </>
   );
