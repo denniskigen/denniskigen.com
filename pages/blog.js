bd1b480a78bd09ce3fdc3afc98ad79d808ae898f
Dennis Kigen <kigen.work@gmail.com>
2022-10-09 23:02:53 +0300

    (feat) Update home page

---
+++ pages/blog.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Container from "../components/Container";
+
+export default function Blog() {
+  return (
+    <Container title="Dennis Kigen - Blog">
+      <article className="flex w-full flex-col items-start justify-center">
+        <h1 className="text-2xl font-semibold text-neutral-600 dark:text-neutral-400">
+          Blog
+        </h1>
+        <p className="mt-8">A collection of my writings on the interwebs.</p>
+      </article>
+      <div className="flex items-center mt-12">
+        <svg
+          className="mx-1"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="19" y1="12" x2="5" y2="12"></line>
+          <polyline points="12 19 5 12 12 5"></polyline>
+        </svg>
+        <Link href="/">
+          <a className="underline underline-offset-4 decoration-zinc-500 hover:decoration-zinc-200">
+            Return home
+          </a>
+        </Link>
+      </div>
+    </Container>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,7 @@ function About() {
   return (
     <div>
       <h1 className="text-2xl font-semibold">Dennis Kigen</h1>
-      <p className="mt-8">
-        Frontend Engineer in love with crafting user interfaces.
-      </p>
+      <p className="mt-8">Frontend Engineer crafting user interfaces.</p>
       <svg
         aria-hidden="true"
         width="80"
@@ -35,35 +33,6 @@ function About() {
       <p className="">
         Building the future of open-source electronic medical records software
         for the developing world at
-        <svg
-          className="mx-2 inline-flex mb-1 h-4 w-4"
-          width="20"
-          height="20"
-          clipRule="evenodd"
-          strokeLinejoin="round"
-          strokeMiterlimit="2"
-          viewBox="0 0 206 233"
-        >
-          <path
-            transform="matrix(.96533 0 0 1 -26.125 -14.65)"
-            d="m217.6 180.46c-6.666-4.186-8.706-12.989-4.529-19.68 4.2-6.681 12.991-8.709 19.672-4.548 6.696 4.2 8.721 12.989 4.56 19.685-4.19 6.672-13.021 8.725-19.703 4.543m13.082-105.33s-5.758 103.5-104.23 172.39c5.013-4.134 71.279-73.305 58.775-85.259-7.929-10.279-33.758-24.929-57.171-32.242 0 0 39.875-3.387 68.296 8.242 4.575-4.337 28.45-37.258 34.329-63.129"
-            fill="#54b947"
-          />
-          <path
-            transform="matrix(.96533 0 0 1 -26.125 -14.65)"
-            d="m38.514 187.16c6.811-3.985 15.552-1.672 19.529 5.125 3.973 6.814 1.67 15.558-5.126 19.521-6.814 3.987-15.551 1.69-19.529-5.116-3.988-6.8-1.669-15.549 5.126-19.53m87.937 60.35s-97.288-51.667-99.388-175.8c1.329 6.346 26.217 100.78 42.621 95.238 12.808-2.242 37.754-18.304 55.129-35.633 0 0-15.55 36.875-39.05 56.641 1.713 6.067 20.479 42.325 40.688 59.555"
-            fill="#71cbd2"
-          />
-          <path
-            transform="matrix(.96533 0 0 1 -26.125 -14.65)"
-            d="M129.035 30.128c-.66 7.86-7.584 13.685-15.441 13-7.836-.646-13.674-7.57-12.998-15.421.662-7.872 7.567-13.674 15.421-13.005 7.853.651 13.683 7.571 13.018 15.426M27.063 71.707s97.363-52.792 203.617 3.413c-5.98-2.596-98.138-28.484-103.075-11.875-5.521 11.745-6.784 41.416-2.604 65.57 0 0-21.196-33.925-23.817-64.533-5.942-2.087-48.417.85-74.121 7.425"
-            fill="#0055a5"
-          />
-        </svg>
-        <ExternalLink href="https://www.ampathkenya.org/">
-          <span className="font-medium">AMPATH</span>
-        </ExternalLink>{" "}
-        and
         <svg
           className="mx-2 inline-flex mb-1 h-4 w-4"
           height="108.174"
@@ -89,6 +58,10 @@ function About() {
         </svg>
         <ExternalLink href="https://openmrs.org/">
           <span className="font-medium">OpenMRS</span>
+        </ExternalLink>
+        . Previously at{" "}
+        <ExternalLink href="https://www.ampathkenya.org/">
+          <span className="font-medium">AMPATH</span>
         </ExternalLink>
         .
       </p>
