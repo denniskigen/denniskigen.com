1f30967aa9f2314dfc7334fb1af2087f7c4b5ad8
Dennis Kigen <kigen.work@gmail.com>
2023-01-29 17:54:06 +0300

    (feat) Add alternative favicons

--- components/Container.js
+++ components/Container.js
@@ -27,6 +27,26 @@ export default function Container(props) {
           href={`https://denniskigen.com${router.asPath}`}
         />
         <link rel="icon" type="image/png" href="favicon.ico?v=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Dennis Kigen" />
         <meta property="og:description" content={meta.description} />
